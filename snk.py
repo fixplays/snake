610f79c5338950494b43a2f399670d5d8bfd1aac
Dmitry <dmitriyomeltschenko@gmail.com>
2021-08-05 22:03:41 +0300

    fury snake

--- snk.py
+++ snk.py
@@ -5,20 +5,23 @@ timer = pg.time.Clock()
 fps = 120
 
 pg.init()
-game_screen = pg.display.set_mode((1920, 1080), pg.OPENGL)
+game_screen = pg.display.set_mode(
+    (800, 600))
+    #,pg.OPENGL)
 
 pg.display.set_caption('Snake')
 game_screen.fill(Color.BLUE)
-work = True
-while work:
-    keys = pg.key.get_pressed()
-    work = not keys[pg.K_ESCAPE]
-    timer.tick(fps)
-    events = pg.event.get()
-    for ev in events:
-        print(ev)
-        if ev.type == pg.WINDOWCLOSE:
-            work = False
+# pg.display.flip()
+# work = True
+# while work:
+#     keys = pg.key.get_pressed()
+#     work = not keys[pg.K_ESCAPE]
+#     timer.tick(fps)
+#     events = pg.event.get()
+#     for ev in events:
+#         print(ev)
+#         if ev.type == pg.WINDOWCLOSE:
+#             work = False
 
 pg.quit()
 
